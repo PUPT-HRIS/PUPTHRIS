3346d1b69e702fd776dbd7c0619a93f47f7204dc
hassan_adnan <adnanhassanda@gmail.com>
2024-07-29 02:17:01 +0800

    add api for updating employee

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -4,5 +4,6 @@ const employeeController = require('../controllers/employeeController');
 const router = express.Router();
 
 router.post('/add', employeeController.addEmployee);
+router.patch('/update/:id', employeeController.updateEmployee);
 
 module.exports = router;
