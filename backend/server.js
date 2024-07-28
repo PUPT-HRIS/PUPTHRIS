da4ea1a72b3c97d008c03ab168f27ef09a4806bc
hassan_adnan <adnanhassanda@gmail.com>
2024-07-28 17:21:48 +0800

    add api for adding employee

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const employeeRoutes = require('./routes/employeeRoutes');
+
+dotenv.config();
+
 const app = express();
-const db = require('./config/db.config');
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/test-db', (req, res) => {
-  db.query('SELECT 1 + 1 AS solution', (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json({ solution: results[0].solution });
-    }
-  });
-});
+ 
+app.use('/api/employees', employeeRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
