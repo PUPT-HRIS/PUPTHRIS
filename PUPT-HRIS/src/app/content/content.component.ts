4b1bfa65ce9aa7f13681188d5a5b7ec2cc02d14c
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-07-31 13:38:37 +0800

    add content page

--- PUPT-HRIS/src/app/content/content.component.ts
+++ PUPT-HRIS/src/app/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EmployeesComponent } from "./employees/employees.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [EmployeesComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
