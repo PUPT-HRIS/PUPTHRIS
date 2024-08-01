c3f319921159bf165a1464c3fbd23c7e7889b6c8
hassan_adnan <adnanhassanda@gmail.com>
2024-08-01 16:52:12 +0800

    create family component

--- PUPT-HRIS/src/app/app.routes.ts
+++ PUPT-HRIS/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { EmployeesComponent } from './pages/employees/employees.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PersonalInfoComponent } from './pages/personal-info/personal-info.component';
 import { EducationComponent } from './pages/education/education.component';
+import { FamilyComponent } from './pages/family/family.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent }, 
   { path: 'employees', component: EmployeesComponent },
   { path: 'personal-info', component: PersonalInfoComponent },
   { path: 'educational-background', component: EducationComponent },
+  { path: 'family-background', component: FamilyComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: '/dashboard' }
 ];
