d7580eda75d2b021cf034b55c70a39537a5845c6
hassan_adnan <adnanhassanda@gmail.com>
2024-08-01 16:44:58 +0800

    create education component

--- PUPT-HRIS/src/app/app.routes.ts
+++ PUPT-HRIS/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { EmployeesComponent } from './pages/employees/employees.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PersonalInfoComponent } from './pages/personal-info/personal-info.component';
+import { EducationComponent } from './pages/education/education.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent }, 
   { path: 'employees', component: EmployeesComponent },
   { path: 'personal-info', component: PersonalInfoComponent },
+  { path: 'educational-background', component: EducationComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: '/dashboard' }
 ];
