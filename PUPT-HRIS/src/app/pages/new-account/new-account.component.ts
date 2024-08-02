6dceb3d5e68c4ef9e50988f926ef6783466e1451
hassan_adnan <adnanhassanda@gmail.com>
2024-08-03 00:26:15 +0800

    add form for new account for super admin

--- PUPT-HRIS/src/app/pages/new-account/new-account.component.ts
+++ PUPT-HRIS/src/app/pages/new-account/new-account.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+// src/app/new-account/new-account.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-new-account',
-  standalone: true,
-  imports: [],
   templateUrl: './new-account.component.html',
-  styleUrl: './new-account.component.css'
+  styleUrls: ['./new-account.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
-export class NewAccountComponent {
+export class NewAccountComponent implements OnInit {
+  newAccountForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {
+    this.newAccountForm = this.fb.group({
+      Fcode: ['', Validators.required],
+      Surname: ['', Validators.required],
+      FirstName: ['', Validators.required],
+      MiddleName: [''],
+      NameExtension: [''],
+      Email: ['', [Validators.required, Validators.email]],
+      EmploymentType: ['', Validators.required],
+      Password: ['', [Validators.required, Validators.minLength(6)]],
+      Role: ['', Validators.required],
+      Department: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.newAccountForm.valid) {
+      this.userService.addUser(this.newAccountForm.value).subscribe({
+        next: response => {
+          console.log('User added successfully', response);
+        },
+        error: error => {
+          console.error('Error adding user', error);
+        }
+      });
+    }
+  }
 }
