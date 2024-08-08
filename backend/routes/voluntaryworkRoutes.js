3516f71b3731fa8c9e3bb7958d61fb5924dbe6e0
hassan_adnan <adnanhassanda@gmail.com>
2024-08-09 00:33:00 +0800

    add delete function to voluntary work

--- backend/routes/voluntaryworkRoutes.js
+++ backend/routes/voluntaryworkRoutes.js
@@ -7,5 +7,6 @@ router.post('/add', voluntaryworkController.addVoluntaryWork);
 router.patch('/update/:id', voluntaryworkController.updateVoluntaryWork);
 router.get('/:id', voluntaryworkController.getVoluntaryWork);
 router.get('/employee/:employeeId', voluntaryworkController.getVoluntaryWorks);
+router.delete('/delete/:id', voluntaryworkController.deleteVoluntaryWork);
 
 module.exports = router;
