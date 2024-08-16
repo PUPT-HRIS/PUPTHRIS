b5f97da4c99c0812de0103504e966c65be2fda0a
hassan_adnan <adnanhassanda@gmail.com>
2024-08-16 16:14:12 +0800

    create model

---
+++ PUPT-HRIS/src/app/model/personal-details.model.ts
@@ -0,0 +1,28 @@
+export interface PersonalDetails {
+  PersonalDetailsID?: number;
+  UserID: number;
+  PlaceOfBirth: string;
+  CivilStatus: string;
+  Height: number;
+  Weight: number;
+  BloodType: string;
+  GSISNumber: string;
+  PagIbigNumber: string;
+  PhilHealthNumber: string;
+  SSSNumber: string;
+  TINNumber: string;
+  AgencyEmployeeNumber: string;
+  Citizenship: string;
+  ResidentialHouseBlockLot: string;
+  ResidentialStreet: string;
+  ResidentialSubdivisionVillage: string;
+  ResidentialBarangay: string;
+  ResidentialCityMunicipality: string;
+  ResidentialProvince: string;
+  PermanentHouseBlockLot: string;
+  PermanentStreet: string;
+  PermanentSubdivisionVillage: string;
+  PermanentBarangay: string;
+  PermanentCityMunicipality: string;
+  PermanentProvince: string;
+}
