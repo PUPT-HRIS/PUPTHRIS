d5452b51189c41ddce1fd8af5a0d871c13d21142
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-08-29 17:01:24 +0800

    create forgot-password component and ui

---
+++ PUPT-HRIS/src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css'],
+})
+export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  submit() {
+    if (this.forgotPasswordForm.valid) {
+      const { email } = this.forgotPasswordForm.value;
+      this.authService.forgotPassword(email)
+        .subscribe({
+          next: (response: any) => {
+            console.log('Password reset link sent:', response.message);
+            this.router.navigate(['/login']);
+          },
+          error: (error) => {
+            console.error('Password reset failed', error);
+            this.errorMessage = 'Failed to send password reset link. Please try again.';
+          }
+        });
+    } else {
+      console.error('Form is invalid');
+      this.errorMessage = 'Please enter a valid email address.';
+    }
+  }
+}
