d3c4dcbd235924a143b61bfd0fa0d76a0da6af42
hassan_adnan <adnanhassanda@gmail.com>
2024-09-05 15:21:29 +0800

    add profile picture fucntion

--- backend/routes/profileImageRoutes.js
+++ backend/routes/profileImageRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const profileImageController = require('../controllers/profileImageController');
 
 router.post('/upload', profileImageController.uploadProfileImage);
-router.get('/:userID', profileImageController.getProfileImage);
-router.delete('/:userID', profileImageController.deleteProfileImage);
+router.get('/:userId', profileImageController.getProfileImage);
 
 module.exports = router;
