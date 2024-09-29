441a7effbb8d025db7e2267cc6cb5a14a054bf84
hassan_adnan <adnanhassanda@gmail.com>
2024-09-29 22:42:12 +0800

    fix empoloyee role

--- PUPT-HRIS/src/app/model/role.model.ts
+++ PUPT-HRIS/src/app/model/role.model.ts
@@ -2,3 +2,10 @@ export interface Role {
   RoleID: number;
   RoleName: string;
 }
+
+export enum RoleName {
+  Faculty = 'faculty',
+  Staff = 'staff',
+  Admin = 'admin',
+  SuperAdmin = 'superadmin'
+}
